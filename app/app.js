53a327fbfdf044829067bbcb27566be749dde459
keanedawg <cfife@chicagoventure.com>
2017-05-06 13:58:37 -0500

    Removed useless code
    Code was part of some test that no longer affects this program's purpose

--- app/app.js
+++ app/app.js
@@ -3,19 +3,6 @@ import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
-
-//var admin = require("ember-firebase-admin");
-//var serviceAccount = require("../ermagerd-5f3ce-firebase-adminsdk-53rlv-7def465afd.json");
-
-//alert(admin);
-
-
-
-//admin.initializeApp({
-//  credential: admin.credential.cert(serviceAccount),
-//  databaseURL: "https://ermagerd-5f3ce.firebaseio.com"
-//});
-
 let App;
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
