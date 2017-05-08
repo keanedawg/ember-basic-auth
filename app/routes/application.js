2d7e0a720a2d10f80acef6e58a39455f733474b8
keanedawg <cfife@chicagoventure.com>
2017-05-08 15:05:54 -0500

    pages now redirected to login by default.
    I consider this prototype complete.

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 export default Ember.Route.extend({
   beforeModel: function() {
+      if(!this.get('session.isAuthenticated')) {
+        this.transitionTo('login');
+      }
       return this.get('session').fetch().catch(function() {});
   },
   actions: {
