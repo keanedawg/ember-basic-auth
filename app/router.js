dab35f93ad41a2ba4ef0592179fd0b6a3a27dec1
keanedawg <cfife@chicagoventure.com>
2017-05-08 13:38:45 -0500

    Secret-Page routes unauthorized users back to login

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.authenticatedRoute('secret-page');
   this.route('login');
   this.route('secret-page');
 });
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,16 +1,11 @@
 import Ember from 'ember';
 export default Ember.Route.extend({
   beforeModel: function() {
-        return this.get('session').fetch().catch(function() {});
+      return this.get('session').fetch().catch(function() {});
   },
   actions: {
-    signIn: function(provider) {
-      this.get('session').open('firebase', { provider: provider}).then(function(data) {
-        console.log(data.currentUser);
-      });
-    },
-    signOut: function() {
-      this.get('session').close();
+    accessDenied: function() {
+      this.transitionTo('login');
     }
   }
 });
